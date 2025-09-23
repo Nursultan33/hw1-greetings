701feb9961ce4e0de5c76a7c08330c09629bb0f8
Nursultan33 <nyrafyra@mail.ru>
2025-09-23 12:13:11 +0600

    another commit

--- src/main/java/kz/kaznu/nursultan/Main.java
+++ src/main/java/kz/kaznu/nursultan/Main.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        allProcedures();
+    }
+
+    public static void allProcedures() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число от 1 до 5");
         int result = scanner.nextInt();
